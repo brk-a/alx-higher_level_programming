820f2fd4516c58126b10b48aa142861d4eda43cb
brk-a <fnjakai@outlook.com>
2021-09-07 12:39:45 +0000

    patches to tasks 1 & 2. made all py files executable

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 
-def print_low_alpha():
+def print_lc_alpha():
         for i in 'abcdefghijklmnopqrstuvwxyz':
-                print('{}'.format(i), end='')
+                print('{}'.format(i), end = '')
 
 if __name__ == '__main__':
-        print_low_alpha()
+        print_lc_alpha()
