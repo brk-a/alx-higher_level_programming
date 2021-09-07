9b189fa1eba1d924c2682013c4561fcc037b8f4e
brk-a <fnjakai@outlook.com>
2021-09-07 14:21:28 +0000

    Tasks 14 - 16: try v 1.0

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def rev_alpha():
+    counter = 0
+    for i in range(122, 96, -1):
+        if i % 2 == 0:
+            counter = i
+        else:
+            counter = i - 32
+    print("{}".format(chr(counter)), end='')
+
+if __name__ == '__main__':
+    rev_alpha()
