37cb72bff611c8e27e93dd543392d24157d568cb
brk-a <fnjakai@outlook.com>
2021-09-08 07:50:49 +0000

    :(

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -2,10 +2,10 @@
 
 
 def print_last_digit(number):
-    last_digit = str(number)
-
-    print('{:d}'.format(last_digit[-1]), end='')
-    return last_digit[-1]
+    str_num = str(number)
+    last_digit = int(str_num[-1])
+    print('{:d}'.format(last_digit), end='')
+    return last_digit
 
 if __name__ == '__main__':
     print_last_digit(98)
