2b20da5d4455edeb265c5db7f649123ffdf9444f
brk-a <fnjakai@outlook.com>
2021-09-09 11:40:10 +0000

    i'm bloody stupid

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -14,9 +14,9 @@ if __name__ == '__main__':
     if num_of_args == 0:
         print('0 arguments.')
     elif num_of_args == 1:
-        print('1 argument.')
+        print('1 argument:')
     else:
-        print('{:d} arguments.'.format(num_of_args))
+        print('{:d} arguments:'.format(num_of_args))
 
     for i, j in enumerate(argv[1:], start=1):
         print('{:d} {:s}'.format(i, j))
--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -7,9 +7,9 @@ Docstring goes here
 """
 
 if __name__ == '__main__':
-    import hidden4
+    import hidden_4
 
-    li = [i for i in dir(hidden4) if not i.startswith('__')]
+    li = [i for i in dir(hidden_4) if not i.startswith('__')]
     li.sort()
     for i in li:
         print('{:s}'.format(i))
