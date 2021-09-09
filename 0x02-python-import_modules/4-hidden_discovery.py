06384c15e3394b0f3dfbf0bbc71d9217f976b6c9
brk-a <fnjakai@outlook.com>
2021-09-09 14:29:28 +0000

    task 4: try v 3.1

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -8,11 +8,12 @@ Docstring goes here
 
 if __name__ == '__main__':
     import marshal
+    import hidden_4
 
     with open('hidden_4.pyc', 'rb') as h_4:
         h_4.seek(8)
         code_obj = marshal.load(h_4)
-        hidden_4 = uncompyle6 -o . hidden_4.pyc
+        #hidden_4 = uncompyle6 -o . hidden_4.pyc
 
     exec(code_obj)
 
