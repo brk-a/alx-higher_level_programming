75ff8b720d5bd19edf50f4098d0a50e0315621df
brk-a <fnjakai@outlook.com>
2021-09-09 09:29:24 +0000

    Task 6: try v 1.0: initial

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+
+"""
+BYOC: Build Yer [not quite] Own Calculator
+
+A basic calculator, written in py.
+
+Ops: add, sub, mul and div.
+
+"""
+
+if __name__ == '__main__':
+    import sys
+    import calculator_1 as calc
+
+    num_of_args = len(sys.argv[1:])
+
+    operators = {
+        '+': calc.add,
+        '-': calc.sub,
+        '*': calc.mul,
+        '/': calc.div}
+
+    def driver():
+        a = int(sys.argv[1])
+        op = sys.argv[2]
+        b = int(sys.argv[3])
+
+        if op not in operators.keys():
+            print('Unknown operator. Available operators: +, -, * and /')
+            sys.exit(1)
+        else:
+            result = operators[op](a, b)
+            print(f'{a} {op} {b} = {result}')
+            return result
+
+    if num_of_args != 3:
+        print('Usage: ./100-my_calculator.py <a> <operator> <b>')
+        sys.exit(1)
+    else:
+        driver()
