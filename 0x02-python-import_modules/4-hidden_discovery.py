bb7c604661c0e6b77d7d4fd12c50ee30016f43c8
brk-a <fnjakai@outlook.com>
2021-09-09 10:45:46 +0000

    voetsek

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -10,5 +10,6 @@ if __name__ == '__main__':
     import hidden4
 
     li = [i for i in dir(hidden4) if not i.startswith('__')]
-    for i in li.sort():
+    li.sort()
+    for i in li:
         print('{:s}'.format(i))
