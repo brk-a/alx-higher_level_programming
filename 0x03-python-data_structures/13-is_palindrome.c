f2874ca54dfdceed24448c2796d6278b76b5e9d4
brk-a <fnjakai@outlook.com>
2021-09-14 07:56:19 +0000

    Task 13: try v 2.0: patch of patch

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * is_palindrome - checks is a list is a palindrome
+ * @head: head of the list
+ *
+ * Return: 1 if the list is a palindrome else 0
+ */
+int is_palindrome(listint_t **head)
+{
+listint_t *current = NULL;
+int nums_[100000], *nums = NULL, i = 0, len = 0;
+
+if (head == NULL || *head == NULL || head[0]->next == NULL)
+return (head != NULL);
+current = *head;
+while (current)
+++len, current = current->next;
+if (len / 2 <= 100000)
+nums = nums_;
+else
+nums = malloc(sizeof(int) * (len / 2));
+current = *head;
+for (i = 0; i < len / 2; ++i)
+nums[i] = current->n, current = current->next;
+if (len % 2)
+current = current->next;
+for (--i; i >= 0; --i, current = current->next)
+if (current->n != nums[i])
+return (free(nums == nums_ ? NULL : nums), 0);
+return (free(nums == nums_ ? NULL : nums), 1);
+}
