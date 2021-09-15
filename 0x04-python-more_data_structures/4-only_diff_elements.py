321eb25466e59c2afd162eac63352bb6e20b4319
brk-a <fnjakai@outlook.com>
2021-09-15 12:51:06 +0000

    Task 4 try v 2.1: caret

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -9,10 +9,7 @@ Docstring goes here
 def common_elements(set_1, set_2):
     set_1 = set_1 if set_1 and type(set_1) == set else set()
     set_2 = set_2 if set_2 and type(set_2) == set else set()
-    common_1 = [i for i in set_1 if i not in set_2]
-    common_2 = [i for i in set_2 if i not in set_1]
-    common = common_1 + common_2
-    return common
+    return set_1 ^ set_2
 
 if __name__ =='__main__':
     set_1 = {'cheesy', 'chilli', 'chunky', 'chutney'}
