f2dd4f745d0bcf1c5576e333d0892a9a604ee0fc
brk-a <fnjakai@outlook.com>
2021-09-15 13:30:37 +0000

    Tasks 13 - 15: try v 1.0

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""
+Docstring goes here
+
+"""
+
+
+def weight_average(my_list=[]):
+    my_list = my_list if my_list and type(my_list) == list else []
+    try:
+        dividend = 0
+        divisor = 0
+        for tup in my_list:
+            dividend += tup[0] * tup[1]
+            divisor += tup[1]
+            return dividend / divisor
+    except ZeroDivisionError:
+        return 0
+
+
+if __name__ == '__main__':
+    my_list = [(1, 2), (2, 1), (3, 10), (4, 2)]
+    # = ((1 * 2) + (2 * 1) + (3 * 10) + (4 * 2)) / (2 + 1 + 10 + 2)
+    result = weight_average(my_list)
+    print("Average: {:0.2f}".format(result))
---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+"""
+Docstring goes here
+
+"""
+
+
+def complex_delete(a_dictionary, value):
+    a_dictionary = a_dictionary if a_dictionary \
+                   and type(a_dictionary) == dict else {}
+    if value not in a_dictionary.values():
+        return a_dictionary
+    else:
+        for k, v in a_dictionary.items():
+            if v == value:
+                a_dictionary.pop(k)
+        return a_dictionary
+
+
+if __name__ == '__main__':
+    print_sorted_dictionary = \
+                              __import__('6-print_sorted_dictionary').print_sorted_dictionary
+
+    a_dictionary = {'lang': "C", 'track': "Low", 'pref': "C", 'ids': [1, 2, 3]}
+    new_dict = complex_delete(a_dictionary, 'C')
+    print_sorted_dictionary(a_dictionary)
+    print("--")
+    print_sorted_dictionary(new_dict)
+
+    print("--")
+    print("--")
+    new_dict = complex_delete(a_dictionary, 'c_is_fun')
+    print_sorted_dictionary(a_dictionary)
+    print("--")
+    print_sorted_dictionary(new_dict)
