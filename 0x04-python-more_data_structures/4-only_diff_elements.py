89ca3d4a43dd45c3f55d459a5a86c4553d40a607
brk-a <fnjakai@outlook.com>
2021-09-16 06:40:05 +0000

    voetsek

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -6,7 +6,7 @@ Docstring goes here
 """
 
 
-def common_elements(set_1, set_2):
+def only_diff_elements(set_1, set_2):
     set_1 = set_1 if set_1 and type(set_1) == set else set()
     set_2 = set_2 if set_2 and type(set_2) == set else set()
     return set_1 ^ set_2
@@ -14,5 +14,5 @@ def common_elements(set_1, set_2):
 if __name__ =='__main__':
     set_1 = {'cheesy', 'chilli', 'chunky', 'chutney'}
     set_2 = {'coleslaw', 'couscous', 'cheesy', 'chunky'}
-    c_set = common_elements(set_1, set_2)
+    c_set = only_diff_elements(set_1, set_2)
     print(sorted(list(c_set)))
