f0af17ee2ad2749af80bf1ba7221183d867e6ef4
brk-a <fnjakai@outlook.com>
2021-09-16 07:03:30 +0000

    voetsek

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -13,7 +13,7 @@ def multiply_by_2(a_dictionary):
     for k, v in new_dict.items():
         new_dict[k] = v * 2
 
-        return new_dict
+    return new_dict
 
 if __name__ == '__main__':
     print_sorted_dictionary = \
