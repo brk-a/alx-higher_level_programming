5f2cf0520b010a512be21ca322f52b273f4a16bb
brk-a <fnjakai@outlook.com>
2021-09-30 04:10:12 -0700

    Tasks 8 and 9: try v 3.0

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 
+"""
+102-square.py
+
+"""
 
 class Square:
     """Square class."""
--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 
+"""
+102-square.py
+
+"""
+
 
 class Square:
     """Square class."""
