1794febc060248ec0ddae0761ac28b065ca46a81
brk-a <fnjakai@outlook.com>
2021-10-01 18:33:59 -0700

    Task 0: try v 1.0

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+'''
+Docstring goes here
+
+'''
+
+
+def add_integer(a, b=98):
+    '''add_int'''
+    if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError('a must be an integer')
+    if not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError('b must be an integer')
+    if isinstance(a, float):
+        a = int(a)
+    if isinstance(b, float):
+        b = int(b)
+
+    return a + b
+
+
+if __name__ == '__main__':
+    add_integer = __import__('0-add_integer').add_integer
+
+    print(add_integer(1, 2))
+    print(add_integer(100, -2))
+    print(add_integer(2))
+    print(add_integer(100.3, -2))
+    try:
+        print(add_integer(4, "School"))
+    except Exception as e:
+        print(e)
+    try:
+        print(add_integer(None))
+    except Exception as e:
+        print(e)
