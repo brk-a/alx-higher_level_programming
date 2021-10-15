871dae263952ed0a1c70d8760d1abbd49b23bdf7
brk-a <fnjakai@outlook.com>
2021-10-15 04:14:49 -0700

    Tasks 0 - 5: try v 1.0

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -42,31 +42,50 @@ class Rectangle(Base):
     @width.setter
     def width(self, width):
         ''' width setter '''
+        if type(width) is not int:
+            raise TypeError(f'width must be an integer')
+        if width <= 0:
+            raise ValueError(f'width must be > 0')
         self.__width = width
 
     @height.setter
     def height(self, height):
         ''' height setter '''
+        if type(height) is not int:
+            raise TypeError(f'height must be an integer')
+        if height <= 0:
+            raise ValueError(f'height must be > 0')
         self.__height = height
 
     @x.setter
     def x(self, x):
         ''' x setter '''
+        if type(x) is not int:
+            raise TypeError(f'x must be an integer')
+        if x < 0:
+            raise ValueError(f'x must be > 0')
         self.__x = x
 
     @y.setter
     def y(self, y):
         ''' y setter '''
+        if type(y) is not int:
+            raise TypeError(f'y must be an integer')
+        if y < 0:
+            raise ValueError(f'y must be > 0')
         self.__y = y
 
+    def area(self):
+        ''' area methode '''
+        return self.__height * self.__width
 
-if __name__ == "__main__":
 
-    r1 = Rectangle(10, 2)
-    print(r1.id)
+if __name__ == '__main__':
+    r1 = Rectangle(3, 2)
+    print(r1.area())
 
     r2 = Rectangle(2, 10)
-    print(r2.id)
+    print(r2.area())
 
-    r3 = Rectangle(10, 2, 0, 0, 12)
-    print(r3.id)
+    r3 = Rectangle(8, 7, 0, 0, 12)
+    print(r3.area())
