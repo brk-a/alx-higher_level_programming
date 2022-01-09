7824b6a89c7107fb38098c0f780a64d4b63a5f76
brk-a <fnjakai@outlook.com>
2022-01-09 00:06:09 -0800

    Task 0: try v 1.3: whitespace :(

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,7 +8,7 @@ fetches https://intranet.hbtn.io/status
 if __name__ == '__main__':
 
     import urllib.request
-    
+
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
         print('Body response:')
