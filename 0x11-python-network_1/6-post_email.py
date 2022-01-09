e5da7e7baec77b46ea62d0cf01a3f3c3a0bc4e79
brk-a <fnjakai@outlook.com>
2022-01-09 00:56:11 -0800

    Tasks 2 - 9: try v 1.0

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+takes in a URL and an email address, sends a POST request
+to the passed URL
+
+"""
+
+if __name__ == '__main__':
+
+    import sys
+    import requests
+
+    payload = {'email': sys.argv[2]}
+    response = requests.post(sys.argv[1], data=payload)
+    print(response.text)
