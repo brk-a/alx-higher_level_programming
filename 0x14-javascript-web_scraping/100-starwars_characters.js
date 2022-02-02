bbb7f0c7d1fd2327db88bd64d81dccc75f94eef9
brk-a <fnjakai@outlook.com>
2022-02-02 03:04:43 -0800

    Task 7: try v 2.0

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -3,26 +3,26 @@
 const request = require('request');
 
 const options = {
-    url: 'http://swapi.co/api/films/' + process.argv[2],
-    headers: {
-	'User-Agent': 'request'
-    }
+  url: 'http://swapi.co/api/films/' + process.argv[1],
+  headers: {
+    'User-Agent': 'request'
+  }
 };
 
 function callback (error, response, body) {
-    if (!error && response.statusCode === 200) {
-	const data = JSON.parse(body);
-	const chars = data['characters'];
-	let i;
-	for (i = 0; i < chars.length; i++) {
+  if (!error && response.statusCode === 200) {
+    const data = JSON.parse(body);
+    const chars = data.characters;
+    let i;
+    for (i = 0; i < chars.length; i++) {
 	    request(chars[i], function (error, response, body) {
-		if (!error && response.statusCode === 200 && body) {
+        if (!error && response.statusCode === 200 && body) {
 		    const cdata = JSON.parse(body);
-		    console.log(cdata['name']);
-		}
+		    console.log(cdata.name);
+        }
 	    });
-	}
     }
+  }
 }
 
 request(options, callback);
